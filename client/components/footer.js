b221baca107e9e68353667a45d315e09afd45ad8
Anton Koller <Anton.Koller@yandex.com>
2017-07-24 17:58:02 +0300

    fix link and useless dependencies

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,3 +1,6 @@
 import React from 'react'
 
-export default () => <div className="footer"> [github] [hash] </div>
+export default () =>
+  <div className="footer">
+    <a href="https://github.com/c0llar/logbook"> [github] </a>
+  </div>
